4d23e60147c200d9ef697986c4398f558b14f382
FattyRatty <60388760+emilasbringer@users.noreply.github.com>
2020-05-19 12:59:55 +0200

    Update KinematicBody2D.gd

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -3,24 +3,55 @@ extends KinematicBody2D
 const UP = Vector2(0, -1)
 var motion = Vector2(0,0)
 export var speed = 1000
+export var jumpheight = -4000
+export var gravity = 600
+var boostspeed = 50000
+var lookingright = true
+var boostammount = 0
+
+
+
+
 
 func _physics_process(delta: float) -> void:
-	motion.y += 10
+	motion.y += gravity
 	
-	if Input.is_action_pressed("ui_right"):
+	
+	if  Input.is_action_pressed("ui_right"):
 		motion.x = speed
+		$Player.flip_h = false
+		get_node("AnimationPlayer").play("Run")
+		lookingright = true
 
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = -speed
-	else: 
+		$Player.flip_h = true
+		get_node("AnimationPlayer").play("Run")
+		lookingright = false
+
+	else:
 		motion.x = 0
-	
+		get_node("AnimationPlayer").play("Idle")
+
+
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
-			motion.y = -400
+			motion.y = jumpheight
+			
+	else: get_node("AnimationPlayer").play("Jump")
+
+
+	if Input.is_action_pressed("R"):
+		 self.position = Vector2(739, 480)
+
+	if motion.y >20000:
+		 self.position = Vector2(739, 480)
+		
+
+	
 
 
 
-	move_and_slide(motion, UP)
+	motion = move_and_slide(motion, UP)
 	pass
 
